49098f96e23b12307b42177095006fb05c87354f
liuling <1372111608@qq.com>
2020-03-03 07:51:16 +0800

    搜索页面完成

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,4 +1,10 @@
+import { HistoryKeyword } from "../../models/history-keyword"
+import {Tag} from "../../models/tag"
+import {Search} from "../../models/search";
+import {showToast} from "../../utils/ui";
+
 // pages/search/search.js
+const history = new HistoryKeyword()
 Page({
 
   /**
@@ -11,8 +17,60 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad:async function (options) {
+    const historyTags = history.get()
+    const hotTags = await Tag.getSearchTags()
+    this.setData({
+      historyTags,
+      hotTags
+    })
 
+  },
+
+  async onSearch(event){
+    this.setData({
+      search:true,
+      items:[]
+    })
+    const keyword = event.detail.value || event.detail.name
+    if (!keyword) {
+      showToast('请输入关键字')
+      return
+  }
+    history.save(keyword)//写入缓存
+    this.setData({
+      historyTags:history.get()
+    })
+    const paging =Search.search(keyword)
+    wx.lin.showLoading({
+      color:'#157658',
+      type:'flash',
+      fullScreen:true
+    })
+    const data = await paging.getMoreData()
+    wx.lin.hideLoading()
+    this.bindItems(data)
+  },
+
+  onCancel(event){
+    this.setData({
+      search:false
+    })
+  },
+
+  bindItems(data){
+    if(data.accumulator.length !== 0){
+      this.setData({
+        items:data.accumulator
+      })
+    }
+  },
+
+  onDeleteHistory(event){
+    history.clear()
+    this.setData({
+      historyTags:[]
+    })
   },
 
   /**
